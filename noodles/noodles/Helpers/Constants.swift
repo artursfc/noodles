6029bd0c9a37a9e7444584051127cf424acc9d68
Pedro Henrique Guedes Silveira <pedrohgsilveira@gmail.com>
2020-01-14 16:18:07 -0300

    Set up Xcode project

---
+++ noodles/noodles/Helpers/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  noodles
+//
+//  Created by Pedro Henrique Guedes Silveira on 14/01/20.
+//  Copyright © 2020 Artur Carneiro. All rights reserved.
+//
+
+import Foundation

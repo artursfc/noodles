ffdf71b3168b6c3f913fd8f2b8d3e3d84b85968f
EdgSgroi <edgar.sgroi@gmail.com>
2020-01-15 15:25:06 -0300

    Models created

---
+++ noodles/noodles/Mocks/ChannelMock.swift
@@ -0,0 +1,13 @@
+//
+//  Channel.swift
+//  noodles
+//
+//  Created by Edgar Sgroi on 14/01/20.
+//  Copyright © 2020 Artur Carneiro. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    <#fields#>
+}

3fcf463df53f00116c101f298310ff5beb76ecd5
EdgSgroi <edgar.sgroi@gmail.com>
2020-01-15 15:47:51 -0300

    Fix folders structure

--- noodles/noodles/Models/Constants.swift
+++ noodles/noodles/Models/Constants.swift
@@ -1,13 +1,9 @@
 //
-//  Channel.swift
+//  Constants.swift
 //  noodles
 //
-//  Created by Edgar Sgroi on 14/01/20.
+//  Created by Pedro Henrique Guedes Silveira on 14/01/20.
 //  Copyright © 2020 Artur Carneiro. All rights reserved.
 //
 
 import Foundation
-
-struct Channel {
-    <#fields#>
-}

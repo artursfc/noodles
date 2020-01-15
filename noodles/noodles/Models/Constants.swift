a19ff09e81b60fd54d1f57ce3fb5672cda087c69
EdgSgroi <edgar.sgroi@gmail.com>
2020-01-15 15:50:15 -0300

    Delete duplicated files

--- noodles/noodles/Models/Constants.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Constants.swift
-//  noodles
-//
-//  Created by Pedro Henrique Guedes Silveira on 14/01/20.
-//  Copyright © 2020 Artur Carneiro. All rights reserved.
-//
-
-import Foundation

8481eb499af91694c9ab63038859f549d033012d
eloisafalcao <eloisamariafalcao@gmail.com>
2020-01-15 14:03:45 -0300

    A complete tabBar with all redesigned icons and all the files of the ViewControllers outside the folders

--- noodles/noodles/CustomTabBarController.swift
+++ noodles/noodles/CustomTabBarController.swift
@@ -13,6 +13,9 @@ class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
 
     let channelsViewController = ChannelsViewController()
     let feedViewController = FeedViewController()
+    let searchViewController = SearchViewController()
+    let saveViewController = SaveViewController()
+    let profileViewController = ProfileViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,22 +28,25 @@ class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
 
         setupTabBarItens()
 
-        let controllers = [channelsViewController, feedViewController]
+        let controllers = [channelsViewController, feedViewController, searchViewController,
+            saveViewController, profileViewController]
+
         self.viewControllers = controllers
     }
 
     func setupTabBarItens() {
-        let channelsIcon = UITabBarItem(title: "",
-                                        image: UIImage(named: "channelTabBarIcon"),
-                                        selectedImage: UIImage(named: "channelTabBarIconSelected"))
+
+        let channelsIcon = UITabBarItem(title: "", image: UIImage(named: "channelTabBarIcon"), tag: 0)
+        let feedIcon = UITabBarItem(title: "", image: UIImage(named: "feedTabBarIcon"), tag: 0)
+        let searchIcon = UITabBarItem(title: "", image: UIImage(named: "searchTabBarIcon"), tag: 0)
+        let saveIcon = UITabBarItem(title: "", image: UIImage(named: "saveTabBarIcon"), tag: 0)
+        let profileIcon = UITabBarItem(title: "", image: UIImage(named: "profileTabBarIcon"), tag: 0)
 
         channelsViewController.tabBarItem = channelsIcon
-
-        let feedIcon = UITabBarItem(title: "",
-                                    image: UIImage(named: "feedTabBarIcon"),
-                                    selectedImage: UIImage(named: "feedTabBarIconSelected"))
-
         feedViewController.tabBarItem = feedIcon
+        searchViewController.tabBarItem = searchIcon
+        saveViewController.tabBarItem = saveIcon
+        profileViewController.tabBarItem = profileIcon
     }
 
     func setup() {

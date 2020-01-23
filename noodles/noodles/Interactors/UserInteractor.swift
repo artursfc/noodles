a0a0ff63fba2480a483b497dd57db535a55f8db4
Artur Carneiro <csf.artur@gmail.com>
2020-01-23 19:36:26 -0300

    Add UserInteractor with CloudKit functions
    CORRECTION: In the previous commit, I claimed that ChannelInteractor
    was done, however I forgot to mention that ONLY CloudKit's
    functions were finished. Core Data functions are still to be done
    as the Core Data Manager needs to be rewritten to make use of
    asynchronous functions.

--- noodles/noodles/Interactors/UserInteractor.swift
+++ noodles/noodles/Interactors/UserInteractor.swift
@@ -10,4 +10,151 @@ import Foundation
 import CloudKit
 
 final class UserInteractor {
+    private let cloudkit: CloudKitManager
+    private let coredata: CoreDataManager
+    private let parser: InteractorParser
+
+    init(cloudkit: CloudKitManager, coredata: CoreDataManager) {
+        self.cloudkit = cloudkit
+        self.coredata = coredata
+        self.parser = InteractorParser()
+    }
+
+    // MARK: Public functions
+    public func fetch(with userID: String, from provider: DataProvider, completionHandler: @escaping ((UserModel?) -> Void)) {
+        switch provider {
+        case .cloudkit:
+            fetch(with: userID) { (user) in
+                if let user = user {
+                    DispatchQueue.main.async {
+                        completionHandler(user)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completionHandler(nil)
+                    }
+                }
+            }
+        case .coredata:
+            DispatchQueue.main.async {
+                completionHandler(nil)
+            }
+        }
+    }
+    
+    public func fetchAll(from provider: DataProvider, completionHandler: @escaping (([UserModel]?) -> Void)) {
+        switch provider {
+        case .cloudkit:
+            let query = cloudkit.generateQuery(of: .users, with: NSPredicate(value: true),
+                                               sortedBy: NSSortDescriptor(key: "creationDate", ascending: false))
+            cloudkit.query(using: query, on: .publicDB) { [weak self] (response) in
+                if response.error != nil {
+                    DispatchQueue.main.async {
+                        completionHandler(nil)
+                    }
+                } else {
+                    if let records = response.records {
+                        let userModels = self?.parser.parse(records: records, into: .users)
+                        if let users = userModels as? [UserModel] {
+                            DispatchQueue.main.async {
+                                completionHandler(users)
+                            }
+                        }
+                    }
+                }
+            }
+        case .coredata:
+            DispatchQueue.main.async {
+                completionHandler(nil)
+            }
+        }
+    }
+
+    public func save(user: UserModel, completionHandler: @escaping ((Bool) -> Void)) {
+        let models = [user]
+        let records = parser.parse(models: models, of: .users)
+        if records.isEmpty {
+            DispatchQueue.main.async {
+                completionHandler(false)
+            }
+        }
+        if let record = records.first {
+            cloudkit.save(record: record, on: .publicDB) { (response) in
+                if response.error == nil && response.records == nil {
+                    DispatchQueue.main.async {
+                        completionHandler(true)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completionHandler(false)
+                    }
+                }
+            }
+        }
+    }
+
+    public func update(user: UserModel, with newUser: UserModel, completionHandler: @escaping ((Bool) -> Void)) {
+        let recordID = CKRecord.ID(recordName: user.id)
+        let models = [newUser]
+        let records = parser.parse(models: models, of: .users)
+        if records.isEmpty {
+            DispatchQueue.main.async {
+                completionHandler(false)
+            }
+        }
+        if let record = records.first {
+            cloudkit.update(recordID: recordID, with: record, on: .publicDB) { (response) in
+                if response.error == nil && response.records == nil {
+                    DispatchQueue.main.async {
+                        completionHandler(true)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completionHandler(false)
+                    }
+                }
+            }
+        }
+    }
+
+    public func delete(user: UserModel, completionHandler: @escaping ((Bool) -> Void)) {
+        let recordID = CKRecord.ID(recordName: user.id)
+        cloudkit.delete(recordID: recordID, on: .publicDB) { (response) in
+            if response.error == nil && response.records == nil {
+                completionHandler(true)
+            } else {
+                completionHandler(false)
+            }
+        }
+    }
+
+    // MARK: Private functions
+    private func fetch(with userID: String, completionHandler: @escaping ((UserModel?) -> Void)) {
+        let recordID = CKRecord.ID(recordName: userID)
+        cloudkit.fetch(recordID: recordID, on: .publicDB) { [weak self] (response) in
+            if response.error == nil {
+                guard let record = response.records?.first else {
+                    return
+                }
+                var user = UserModel(id: userID, name: record["name"] ?? "", rank: nil, createdAt: record["createdAt"] ?? nil, editedAt: record["modifiedAt"] ?? nil)
+                if let rankRef = record["rank"] as? CKRecord.Reference {
+                    self?.cloudkit.fetch(recordID: rankRef.recordID, on: .publicDB, completionHandler: { [weak self] (response) in
+                        if let record = response.records {
+                            let rankModel = self?.parser.parse(records: record, into: .ranks)
+                            if let rank = rankModel?.first as? RankModel {
+                                user.rank = rank
+                                DispatchQueue.main.async {
+                                    completionHandler(user)
+                                }
+                            }
+                        }
+                    })
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completionHandler(nil)
+                }
+            }
+        }
+    }
 }

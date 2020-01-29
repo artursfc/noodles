56f49c5d09a4b7f2d49b1109fcabfc69ba0d0a43
eloisafalcao <eloisamariafalcao@gmail.com>
2020-01-28 23:22:20 -0300

    ChannelCreationViewController almost done, but it need to be tested

--- noodles/noodles/SceneDelegate.swift
+++ noodles/noodles/SceneDelegate.swift
@@ -11,22 +11,23 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
- 
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let navController = UINavigationController()
+        let mainCoordinator = MainCoordinator(navController: navController)
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = CustomTabBarController()
-        window?.makeKeyAndVisible()
+        let mainView = CustomTabBarController()
+        navController.viewControllers = [mainView]
+        self.window?.rootViewController = navController
+        self.window?.makeKeyAndVisible()
 
-        let navController = UINavigationController()
-
-        let mainCoordinator = MainCoordinator(navController: navController)
-        mainCoordinator.start()
+         mainCoordinator.start()
 
     }
     
--- noodles/noodles/View + ViewModel/ChannelCreation/ChannelCreationViewController.swift
+++ noodles/noodles/View + ViewModel/ChannelCreation/ChannelCreationViewController.swift
@@ -10,38 +10,114 @@ import Foundation
 import UIKit
 
 class ChannelCreationViewController: UIViewController {
-    
-    @IBOutlet weak var rankCanEditTableView: UITableView!
-    @IBOutlet weak var rankCanViewTableView: UITableView!
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+
+    @IBOutlet weak var rankTableView: UITableView!
     var RANKSELECTIONTABLEVIEWCELL = "RankSelectionTableViewCell"
-    let dataSourceCanEdit = RankSelectionTableViewDataSource()
-    let delegateCanEdit = RankSelectionTableViewDelegate()
-    let dataSourceCanView = RankSelectionTableViewDataSource()
-    let delegateCanView = RankSelectionTableViewDelegate()
-    var selectedRanks: [RankModel] = [RankModel]()
+    private let viewModel: AddChannelViewModel
+    private let coordinator: Coordinator
+    var selectedRanksThatCanEditIndex: [Int] = []
+    var selectedRanksThatCanViewIndex: [Int] = []
     
-//    init(viewModel: ChannelCreationViewModel, viewController: ViewController) {
-//        <#statements#>
-//    }
+    init(viewModel: AddChannelViewModel, coordinator: Coordinator) {
+        self.viewModel = viewModel
+        self.coordinator = coordinator
+        super.init(nibName: "ChannelCreationViewController", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupRankCanEditTableView()
-        setupRankCanViewTableView()
+        setupNavController()
+        setupRankTableView()
         view.backgroundColor = UIColor.fakeWhite
     }
 
-    func setupRankCanEditTableView() {
-        rankCanEditTableView.dataSource = dataSourceCanEdit
-        rankCanEditTableView.delegate = delegateCanEdit
-        rankCanEditTableView.register(UINib(nibName: RANKSELECTIONTABLEVIEWCELL, bundle: nil), forCellReuseIdentifier: RANKSELECTIONTABLEVIEWCELL)
-        rankCanEditTableView.separatorColor = UIColor.clear
+    func setupRankTableView() {
+        rankTableView.dataSource = self
+        rankTableView.delegate = self
+        rankTableView.register(UINib(nibName: RANKSELECTIONTABLEVIEWCELL, bundle: nil), forCellReuseIdentifier: RANKSELECTIONTABLEVIEWCELL)
+        rankTableView.separatorColor = UIColor.clear
     }
-    
-    func setupRankCanViewTableView() {
-        rankCanEditTableView.dataSource = dataSourceCanView
-        rankCanEditTableView.delegate = delegateCanView
-        rankCanEditTableView.register(UINib(nibName: RANKSELECTIONTABLEVIEWCELL, bundle: nil), forCellReuseIdentifier: RANKSELECTIONTABLEVIEWCELL)
-        rankCanEditTableView.separatorColor = UIColor.clear
+
+    func setupNavController() {
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButton))
+        let navItem = UINavigationItem(title: "Done")
+        navItem.rightBarButtonItem = doneItem
+        navigationController?.navigationBar.setItems([navItem], animated: false)
+
+    }
+
+    @objc func doneButton(){
+        viewModel.name = titleTextField.text ?? "Canal"
+        viewModel.canBeEditedBy = selectedRanksThatCanEditIndex
+        viewModel.canBeViewedBy = selectedRanksThatCanViewIndex
+        viewModel.create()
     }
 }
+
+extension ChannelCreationViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.ranksNumberOfRows()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RANKSELECTIONTABLEVIEWCELL) as? RankSelectionTableViewCell ?? RankSelectionTableViewCell()
+
+        cell.checkImg.image = UIImage(named: "checkPlaceHolder")
+
+        cell.rankTitle.text = viewModel.rankNames(at: indexPath.row)
+        return cell
+    }
+}
+
+extension ChannelCreationViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Can edit this channel: "
+        case 1:
+            return "Can view this channel: "
+        default:
+            return " "
+        }
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            selectedRanksThatCanViewIndex.append(indexPath.row)
+        case 1:
+            selectedRanksThatCanEditIndex.append(indexPath.row)
+        default:
+            break
+        }
+    }
+}
+
+extension ChannelCreationViewController: AddChannelViewModelDelegate {
+
+    func reject(field: AddChannelField) {
+        // criar situação de rejeitado
+    }
+
+    func accept(field: AddChannelField) {
+        // criar situação de aceito
+    }
+
+    func saved() {
+        //mandar dados para Coordinators
+    }
+}
+
